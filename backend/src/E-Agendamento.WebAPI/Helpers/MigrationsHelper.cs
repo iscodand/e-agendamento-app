a5111c1397e31da4b2b76d8e1fc74cc5c5ee11ce
iscodand <isco.dev@outlook.com>
2024-05-06 01:08:26 -0300

    feature: adicionando helpers de db

---
+++ backend/src/E-Agendamento.WebAPI/Helpers/MigrationsHelper.cs
@@ -0,0 +1,4 @@
+namespace E_Agendamento.WebAPI.Helpers
+{
+
+}
---
+++ backend/src/E-Agendamento.WebAPI/Helpers/SeedHelper.cs
@@ -0,0 +1,37 @@
+using E_Agendamento.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace E_Agendamento.WebAPI.Helpers
+{
+    public static class SeedHelper
+    {
+        public static async Task SeedDatabaseAsync(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            {
+                try
+                {
+
+                    UserManager<ApplicationUser> userManager = (UserManager<ApplicationUser>)scope
+                        .ServiceProvider
+                        .GetService(typeof(UserManager<ApplicationUser>));
+
+                    // ICompanyService companyService = (ICompanyService)scope
+                    //     .ServiceProvider
+                    //     .GetService(typeof(ICompanyService));
+
+                    RoleManager<ApplicationRole> roleManager = (RoleManager<ApplicationRole>)scope
+                        .ServiceProvider
+                        .GetService(typeof(RoleManager<ApplicationRole>));
+
+                    await Infrastructure.Identity.Seeds.DefaultSuperAdmin.SeedAsync(userManager, new CancellationToken());
+                    await Infrastructure.Identity.Seeds.DefaultRoles.SeedAsync(roleManager, new CancellationToken());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ops! An Error ocurred while seeding database: {ex}");
+                }
+            }
+        }
+    }
+}
