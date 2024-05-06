2996def6537d894282aa1f6ad12783fd6f309020
iscodand <isco.dev@outlook.com>
2024-05-06 16:07:25 -0300

    fix: metodo generico find tem que ter o Id default como string

--- backend/src/E-Agendamento.Application/Contracts/Repositories/Common/IGenericRepository.cs
+++ backend/src/E-Agendamento.Application/Contracts/Repositories/Common/IGenericRepository.cs
@@ -5,7 +5,7 @@ namespace E_Agendamento.Application.Contracts.Repositories.Common
         public Task<ICollection<T>> GetAllAsync();
         public Task<IReadOnlyList<T>> GetPagedResponseAsync(int pageNumber, int pageSize);
         public Task<int> GetTotalItemsCountAsync();
-        public Task<T> GetByIdAsync(int id);
+        public Task<T> GetByIdAsync(string id);
         public Task<T> CreateAsync(T entity);
         public Task AddAsync(T entity);
         public void UpdateWithoutSave(T entity);
