41a274972a00ef27139b1cb25776dfdb0d5a6699
iscodand <isco.dev@outlook.com>
2024-05-06 01:05:26 -0300

    feature: adicionando validação de campos para autenticação

--- backend/src/E-Agendamento.Application/DTOs/Account/AuthenticationRequest.cs
+++ backend/src/E-Agendamento.Application/DTOs/Account/AuthenticationRequest.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace E_Agendamento.Application.DTOs.Account
 {
     public class AuthenticationRequest
     {
+        [Required(ErrorMessage = "Seu nome de usuário é obrigatório.")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Sua senha é obrigatória.")]
         public string Password { get; set; }
     }
 }
