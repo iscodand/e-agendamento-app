d86c16eb27ef692c292080ba85e19bcc10acc58f
iscodand <isco.dev@outlook.com>
2024-05-19 22:19:58 -0300

    feature: adicionando mais componentes e organizando estrutura de pastas

---
+++ frontend/src/src/components/ui/buttons/ActionButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <button type="button" :class="buttonClasses">
+            <slot />
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue';
+
+// Define as propriedades e seus tipos
+const props = defineProps({
+    color: {
+        type: String as () => 'blue' | 'green' | 'light-green' | 'red' | 'yellow' | 'disabled' | 'default',
+        default: 'default'
+    }
+    type: {
+        type: sumbit
+    }
+});
+
+// Computed property para calcular as classes com base na propriedade color
+const buttonClasses = computed(() => {
+    return {
+        'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800': props.color === 'blue',
+        'focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800': props.color === 'green',
+        'focus:outline-none text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800': props.color === 'light-green',
+        'focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900': props.color === 'red',
+        'focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:focus:ring-yellow-900': props.color === 'yellow',
+        'cursor-disabled text-gray-500 bg-gray-200 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700': props.color === 'disabled',
+        'border border-none focus:outline-none hover:bg-gray-200 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700': props.color === 'default',
+    };
+});
+</script>
--- frontend/src/src/components/ui/layout/MainComponent.vue
+++ frontend/src/src/components/ui/layout/MainComponent.vue
@@ -1,4 +1,4 @@
-<script  lang="ts" setup>
+<script lang="ts" setup>
 import NavComponent from './NavComponent.vue'
 
 // export default {
