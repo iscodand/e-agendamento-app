dd7c9036571c030388e14988cdcd90562965672e
iscodand <isco.dev@outlook.com>
2024-06-21 08:37:07 -0300

    feature: adicionando metodos de post e update

--- backend/src/E-Agendamento.WebAPI/Controllers/v1/CategoryController.cs
+++ backend/src/E-Agendamento.WebAPI/Controllers/v1/CategoryController.cs
@@ -1,7 +1,8 @@
+using E_Agendamento.Application.Features.Categories.Commands.CreateCategory;
+using E_Agendamento.Application.Features.Categories.Commands.UpdateCategory;
 using E_Agendamento.Application.Features.Categories.Queries.GetCategoriesByCompany;
 using E_Agendamento.WebAPI.Controllers.Common;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Agendamento.WebAPI.Controllers.v1
@@ -13,12 +14,35 @@ namespace E_Agendamento.WebAPI.Controllers.v1
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok(await Mediator.Send(
-                new GetCategoriesByCompanyQuery()
-                {
-                    CompanyId = AuthenticatedUser.CompanyId
-                }
-            ));
+            GetCategoriesByCompanyQuery query = new()
+            {
+                CompanyId = AuthenticatedUser.CompanyId
+            };
+
+            if (query.CompanyId is null)
+                return StatusCode(StatusCodes.Status401Unauthorized, "Você não tem permissão.");
+
+            return Ok(await Mediator.Send(query));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateCategoryCommand command)
+        {
+            command.CompanyId = AuthenticatedUser.CompanyId;
+            return StatusCode(StatusCodes.Status201Created, await Mediator.Send(command));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, [FromBody] UpdateCategoryCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            command.CompanyId = AuthenticatedUser.CompanyId;
+
+            return Ok(await Mediator.Send(command));
         }
     }
 }
