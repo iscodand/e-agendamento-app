136bdc4129854614b1027f2e9979ac5d5bd83279
iscodand <isco.dev@outlook.com>
2024-07-22 09:29:35 -0300

    feature: iniciando implementacao de recuperar schedules por usuario autenticado

---
+++ backend/src/E-Agendamento.Application/Features/Schedules/Queries/GetSchedulesByUser/GetSchedulesByUserViewModel.cs
@@ -0,0 +1,37 @@
+using E_Agendamento.Domain.Entities;
+
+namespace E_Agendamento.Application.Features.Schedules.Queries.GetSchedulesByUser
+{
+    public class GetSchedulesByUserViewModel
+    {
+        public string Id { get; set; }
+        public string ItemId { get; set; }
+        public string Observation { get; set; }
+        public string Status { get; set; }
+
+        // must be UserResponse
+        public string RequestedBy { get; set; }
+
+        // must be UserResponse
+        public string ConfirmedBy { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public string CompanyId { get; set; }
+
+        public static IEnumerable<GetSchedulesByUserViewModel> Map(IEnumerable<Schedule> schedules)
+        {
+            return schedules.Select(x => new GetSchedulesByUserViewModel
+            {
+                Id = x.Id,
+                ItemId = x.ItemId,
+                Observation = x.Observation,
+                Status = x.Status,
+                RequestedBy = x.RequestedById,
+                ConfirmedBy = x.ConfirmedById,
+                StartedAt = x.StartAt,
+                EndAt = x.EndAt,
+                CompanyId = x.CompanyId
+            });
+        }
+    }
+}
