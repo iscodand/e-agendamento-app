d1aaa670a040c583eb177630a1f4981acc9a5ae3
iscodand <isco.dev@outlook.com>
2024-08-02 11:06:10 -0300

    feature: melhorando estilização do componente

--- frontend/src/src/components/ui/dashboard/CardComponent.vue
+++ frontend/src/src/components/ui/dashboard/CardComponent.vue
@@ -7,11 +7,11 @@ defineProps<{ title: string, subtitle?: string, stat: number, icon: string }>()
 </script>
 
 <template>
-    <Card style="min-width: 17rem; overflow: hidden; background-color: #10b981;"
+    <Card style="width: 20rem; overflow: hidden; background-color: #3b82f6;"
         class="transition ease-in-out delay-50 shadow hover:shadow-2xl">
         <template #title>
             <div class="text-center">
-                <i :class="['pi', icon, 'm-2']" style="font-size: 1.5rem; color: #e5e7eb;"></i>
+                <i :class="['pi', icon, 'm-2']" style="font-size: 1.5rem; color: #b0cdfd;"></i>
             </div>
             <div class="text-center">
                 <p class="text-white">{{ title }}</p>
